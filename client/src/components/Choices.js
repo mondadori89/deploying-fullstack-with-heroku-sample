082bc4d14c33385a099fc7a801e5174f8cce9ed2
Monda <lmondadori@gmail.com>
2022-08-27 22:34:45 -0300

    Fix: api 6th try

--- client/src/components/Choices.js
+++ client/src/components/Choices.js
@@ -4,7 +4,7 @@ const Choices = ({ handleNewActivity, handleAddActivity, name }) => {
 
   return (
     <div>
-      <button id="primary-btn" onClick={() => handleNewActivity()}>No thanks...</button>
+      <button id="primary-btn" onClick={() => handleNewActivity()}>Noooooo thanks...</button>
       <button id="success-btn" onClick={() => handleAddActivity(name)}>Sounds fun!</button>
       
     </div>
